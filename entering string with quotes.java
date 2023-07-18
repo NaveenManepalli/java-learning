75b726ce519680795c2c32c083a5b63bddcccc97
Naveen.B <86225990+NaveenManepalli@users.noreply.github.com>
2023-07-18 19:00:25 +0530

    Update entering string with quotes.java

--- entering string with quotes.java
+++ entering string with quotes.java
@@ -6,6 +6,6 @@ import java.util.*;
 public class Solution {
 
     public static void main(String[] args) {
-       System.out.println("\"Hello, World!\"");
+       System.out.println("\"Hello, World! I am naveen\"");
     }
 }
